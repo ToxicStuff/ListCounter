5830eabd8facba883e052ca90326d8ae64e29f51
XoX-Toxic <77903650+ToxicStuff@users.noreply.github.com>
2022-08-03 01:23:56 +0200

    Add files via upload

---
+++ CounterListMod.cs
@@ -0,0 +1,35 @@
+﻿using MelonLoader;
+using System.Collections;
+using System;
+using UnhollowerRuntimeLib;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC;
+using VRC.Core;
+
+[assembly: MelonInfo(typeof(ListCounter.Main), ListCounter.BuildInfo.Name, ListCounter.BuildInfo.Version, ListCounter.BuildInfo.Author, ListCounter.BuildInfo.DownloadLink)]
+[assembly: MelonGame("VRChat", "VRChat")]
+
+namespace ListCounter;
+
+public static class BuildInfo
+{
+    public const string Name = "ListCounter";
+    public const string Author = "XoX-Toxic";
+    public const string Version = "1.0.0";
+    public const string DownloadLink = "https://github.com/ToxicStuff/ListCounter";
+}
+
+public class Main : MelonMod 
+{
+    public override void OnApplicationStart()
+    {
+        MelonCoroutines.Start(DoTheUi());
+    }
+    
+    private IEnumerator DoTheUi() 
+    {
+        while (VRCUiManager.prop_VRCUiManager_0 == null) yield return new WaitForEndOfFrame();
+        Patch.Init();
+    }
+}
---
+++ Patch.cs
@@ -0,0 +1,46 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using VRC;
+using VRC.Core;
+
+namespace ListCounter
+{
+    internal class Patch
+    {
+        public static HarmonyLib.Harmony harmonyInstance = new HarmonyLib.Harmony("Patches");
+        public static void Init()
+        {
+            foreach (MethodInfo methodInfo in typeof(UiUserList).GetMethods())
+            {
+                if (methodInfo.Name.StartsWith("Method_Protected_Virtual_Void_VRCUiContentButton_Object_"))
+                    harmonyInstance.Patch(methodInfo, null, new HarmonyMethod(typeof(Patch).GetMethod(nameof(UiUserListPrefix), BindingFlags.Static | BindingFlags.NonPublic)));
+            }
+        }
+        private static void UiUserListPrefix(ref UiUserList __instance, ref VRCUiContentButton __0, ref Il2CppSystem.Object __1)
+        {
+            if (__instance.Cast<UiUserList>() != null)
+            {
+                UiUserList list = __instance.Cast<UiUserList>();
+                var txt = list.field_Public_Text_0;
+                string Text = txt.text;
+                if (Text.Contains("In Room"))
+                {
+                    txt.text = $"In Room [{PlayerManager.field_Private_Static_PlayerManager_0.field_Private_List_1_Player_0.Count}/{RoomManager.field_Internal_Static_ApiWorld_0.capacity}]";
+                }
+                else if (Text.Contains("Online Friends"))
+                {
+                    txt.text = $"Online Friends [{list.field_Private_Int32_0}/{APIUser.CurrentUser.friendIDs.Count}]";
+                }
+                else if (Text.Contains("Offline Friends"))
+                {
+                    txt.text = $"Offline Friends [{list.field_Private_Int32_0}]";
+                }
+            }
+        }
+    }
+}
